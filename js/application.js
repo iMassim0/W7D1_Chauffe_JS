0989c4e1b53ab5fa7b9cada4f94141e2515e7a0c
iMassim0 <i.max@live.fr>
2018-05-21 21:07:39 +0200

    first commit + fct ok

---
+++ js/application.js
@@ -0,0 +1,47 @@
+var a = "Hello World";
+console.log(a);
+
+var test = 99;
+
+while (test =! 0) {
+  var test = Number(prompt("Que souhaitez-vous faire?\n1:Jouer avec les chiffres\n2:Jouer avec les lettres\n0:Quitter"));
+  if (test === 1) {
+    var choice = Number(prompt("Combien de nombre voulez-vous rentrer ?"));
+    console.log(choice);
+    var array = [];
+    var i;
+    for (i = 0; i < choice ; i++) {
+        var temp = Number(prompt(i+1 + "e numero :"));
+        array.push(temp);
+    };
+    console.log(array);
+    mymax(array)
+    reverse(array);
+    console.log(array);
+  } else if (test === 2) {
+    var choice = String(prompt("Rentrez un mot :"));
+    reverse(choice);
+    vowel(choice);
+  } else if (test === 0) {
+    console.log("Goodbye!");
+    break;
+  } else {
+    ;
+  };
+};
+
+function mymax(arr) {
+    return console.log("chiffre le plus grand du tableau = " + Math.max(...arr) )
+};
+
+function reverse(som) {
+  arr = som.split('');
+  arr.reverse();
+  return console.log(arr.join(''));
+};
+
+function vowel(som) {
+  var regex = /[aeiou]/gi;
+  var lettres = som.match(regex);
+  return console.log("Il y a " + lettres.length + " voyelles");
+};
